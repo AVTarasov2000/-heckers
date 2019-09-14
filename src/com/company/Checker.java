717a0fee389feb5612147048e2c3d00cd3668f96
andrejtarasov <tarasov_av@cs.vsu.ru>
2019-09-14 11:53:14 +0300

    first commit

---
+++ src/com/company/Checker.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Checker {
+
+    private int x;
+    private int y;
+    private boolean isQueen=false;
+    private boolean isWhite;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isQueen() {
+        return isQueen;
+    }
+
+    public Checker(int x, int y, boolean isWhite){
+        this.x=x;
+        this.y=y;
+        this.isWhite = isWhite;
+    }
+
+    public void goTo(int x, int y){
+        this.x=x;
+        this.y=y;
+        if(isWhite){
+            if(y==7){
+                isQueen=true;
+            }
+        } else {
+            if (y==0){
+                isQueen=true;
+            }
+        }
+    }
+
+    public boolean isOnCell(int x,int y){
+        return (this.x==x && this.y==y);
+    }
+
+    public int getXDirection(){
+        if(isWhite){
+            return 1;
+        }else {
+            return -1;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Checker{" +
+                "x=" + x +
+                ", y=" + y +
+                ", isQueen=" + isQueen +
+                '}';
+    }
+}
